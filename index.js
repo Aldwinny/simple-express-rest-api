a6238d6e3684baf7acc07ee5c827cdbd4ff091b4
Aldwinny <aldwin0313@yahoo.com.ph>
2023-02-06 07:53:12 +0800

    test

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get("/", (req, res, next) => {
   res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
-app.listen(80, () => {
+app.listen(3000, () => {
   console.log("Server running on port 443");
 });
